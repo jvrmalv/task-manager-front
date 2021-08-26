1512263a49c123e4db054bc0e10c5ca88248a881
jvrmalv <jvrmalv@gmail.com>
2021-08-26 20:54:45 -0300

    feat:add Task functionality

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,31 +1,71 @@
 import { Task, State } from "../pages/TaskOrganizer";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 type Props = {
   list: State;
+  idGet: Function;
+  deleteTask: Function;
 };
 
 export function Tasks(props: Props) {
-  return (
-    <Container className="bg-dark">
-      <Row>
-        <Col className="text-light">Name</Col>
-        <Col className="text-light">Completed</Col>
-      </Row>
-      <Row className="bg-secondary">
-        <Col>
-          {props.list.tasks.map((obj: Task) => {
-            return (
-              <Row key={obj.id}>
-                <Col className="text-light">{obj.name}</Col>
-                <Col className="text-light">
-                  {obj.completed ? "true" : "false"}
-                </Col>
-              </Row>
-            );
-          })}
-        </Col>
-      </Row>
-    </Container>
-  );
+  if (props.list.filterCompleted) {
+    return (
+      <Container className="bg-dark">
+        <Row>
+          <Col className="text-light">Name</Col>
+          <Col className="text-light">Completed</Col>
+        </Row>
+        <Row className="bg-secondary">
+          <Col>
+            {props.list.tasks.filter((v) => v.completed).map((obj: Task) => {
+              return (
+                <Row key={obj.id}>
+                  <Col className="text-light">{obj.name}</Col>
+                  <Col className="text-light">
+                    {obj.completed ? "true" : "false"}
+                  </Col>
+                  <Col>
+                    <Button onClick={() => props.idGet(obj.id, obj.name,)}>Edit</Button>
+                  </Col>
+                  <Col>
+                    <Button onClick={() => props.deleteTask(obj.id)}>DELETE</Button>
+                  </Col>
+                </Row>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+  else {
+    return (
+      <Container className="bg-dark">
+        <Row>
+          <Col className="text-light">Name</Col>
+          <Col className="text-light">Completed</Col>
+        </Row>
+        <Row className="bg-secondary">
+          <Col>
+            {props.list.tasks.map((obj: Task) => {
+              return (
+                <Row key={obj.id}>
+                  <Col className="text-light">{obj.name}</Col>
+                  <Col className="text-light">
+                    {obj.completed ? "true" : "false"}
+                  </Col>
+                  <Col>
+                    <Button onClick={() => props.idGet(obj.id, obj.name,)}>Edit</Button>
+                  </Col>
+                  <Col>
+                    <Button onClick={() => props.deleteTask(obj.id)}>DELETE</Button>
+                  </Col>
+                </Row>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    )
+  };
 }
--- src/pages/TaskOrganizer.tsx
+++ src/pages/TaskOrganizer.tsx
@@ -6,7 +6,6 @@ import {
   Col,
   Button,
   Modal,
-  InputGroup,
   Form,
 } from "react-bootstrap";
 import { Tasks } from "../components/Tasks";
@@ -21,8 +20,26 @@ export type State = {
   loading: boolean;
   tasks: Array<Task>;
   error: string;
+  createInputValue: string;
+  updateInputValue: string;
+  updateCheckValue: boolean;
+  searchValue: string;
+  currID: string;
   modal: boolean;
-  modalInput: string;
+  filterCompleted: boolean
+};
+
+const initialState = {
+  loading: false,
+  tasks: [],
+  error: "",
+  createInputValue: "",
+  updateInputValue: "",
+  updateCheckValue: false,
+  searchValue: "",
+  currID: "",
+  modal: false,
+  filterCompleted: false
 };
 
 type Action =
@@ -30,52 +47,82 @@ type Action =
   | { type: "FETCH_ERROR" }
   | { type: "MODAL_TOGGLE" }
   | {
-      type: "NEW_TASK";
-      get: [];
-    }
-  | { type: "VALUE"; newValue: string };
+    type: "NEW_TASK";
+    get: [];
+  }
+  | { type: "CREATE_INPUT_VALUE"; newValue: string }
+  | { type: "UPDATE_INPUT_VALUE"; newValue: string }
+  | { type: "UPDATE_CHECK_VALUE"; newValue: boolean }
+  | { type: "SEARCH_VALUE"; newValue: string }
+  | { type: "CURR_ID"; newValue: string }
+  | { type: "MODAL_TOGGLE" }
+  | { type: "COMPLETED_TOGGLE" };
 
-const initialState = {
-  loading: false,
-  tasks: [],
-  error: "",
-  modal: false,
-  modalInput: "",
-};
+
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "FETCH_SUCCESS":
       return {
+        ...state,
         loading: false,
         tasks: action.payload,
         error: "",
-        modal: false,
-        modalInput: "",
-      };
+        createInputValue: "",
+        updateInputValue: "",
+        updateCheckValue: false,
+        searchValue: "",
+        currID: "",
+        modal: false
+      }
     case "FETCH_ERROR":
       return {
+        ...state,
         loading: false,
         tasks: [],
         error: "",
-        modal: false,
-        modalInput: "",
+        createInputValue: "",
+        updateInputValue: "",
+        updateCheckValue: false,
+        searchValue: "",
+        currID: "",
+        modal: false
       };
+    case "CREATE_INPUT_VALUE":
+      return {
+        ...state,
+        createInputValue: action.newValue
+      };
+    case "UPDATE_INPUT_VALUE":
+      return {
+        ...state,
+        updateInputValue: action.newValue
+      };
+    case "UPDATE_CHECK_VALUE":
+      return {
+        ...state,
+        updateCheckValue: action.newValue
+      };
+    case "SEARCH_VALUE":
+      return {
+        ...state,
+        searchValue: action.newValue
+      };
+    case "CURR_ID":
+      return {
+        ...state,
+        currID: action.newValue
+      }
     case "MODAL_TOGGLE":
       return {
         ...state,
-        modal: state.modal ? false : true,
-      };
-    case "NEW_TASK":
+        modal: state.modal ? false : true
+      }
+    case "COMPLETED_TOGGLE":
       return {
         ...state,
-        tasks: action.get,
-      };
-    case "VALUE":
-      return {
-        ...state,
-        modalInput: action.newValue,
-      };
+        filterCompleted: state.filterCompleted ? false : true
+      }
     default:
       return state;
   }
@@ -84,15 +131,66 @@ const reducer = (state: State, action: Action): State => {
 function TaskOrganizer() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+
+
   const submitHandler = () => {
-    axios
-      .post("/task", { name: state.modalInput, completed: false })
-      .then(() => {
-        axios.get("/task").then((response) => {
-          dispatch({ type: "NEW_TASK", get: response.data });
-        });
-      });
+    if (state.createInputValue !== "") {
+      axios
+        .post("/task", { name: state.createInputValue, completed: false })
+        .then(() => {
+          axios.get("/task").then((response) => {
+            dispatch({ type: "FETCH_SUCCESS", payload: response.data });
+            console.log(response.data)
+          })
+            .catch((error) => dispatch({ type: "FETCH_ERROR" }));
+        })
+    }
   };
+
+  const updateModal = (id: string, name: string, completed: boolean) => {
+    dispatch({ type: "CURR_ID", newValue: id })
+    dispatch({ type: "UPDATE_INPUT_VALUE", newValue: name })
+    dispatch({ type: "UPDATE_CHECK_VALUE", newValue: completed })
+    dispatch({ type: "MODAL_TOGGLE" })
+  }
+
+  const updateHandler = () => {
+    axios
+      .put(`/task/${state.currID}`, { name: state.updateInputValue, completed: state.updateCheckValue })
+      .then(() => axios.get('/task').then((response) => {
+        dispatch({ type: "FETCH_SUCCESS", payload: response.data })
+      }))
+      .catch((error) => {
+        dispatch({ type: "FETCH_ERROR" })
+      })
+  }
+
+  const deleteTask = (id: string) => {
+    axios
+      .delete(`/task/${id}`)
+      .then(() => {
+        axios
+          .get('/task')
+          .then((response) => dispatch({ type: "FETCH_SUCCESS", payload: response.data }))
+      })
+  }
+
+  const searchTasks = () => {
+    if (state.searchValue !== "") {
+      axios
+        .get(`/task/?name=${state.searchValue}`)
+        .then((response) => {
+          dispatch({ type: "FETCH_SUCCESS", payload: response.data })
+        })
+        .catch((error) => {
+          dispatch({ type: "FETCH_ERROR" })
+        })
+    } else {
+      axios
+        .get('/task')
+        .then((response) => dispatch({ type: "FETCH_SUCCESS", payload: response.data }))
+    }
+  }
   useEffect(() => {
     axios
       .get("/task")
@@ -103,43 +201,35 @@ function TaskOrganizer() {
         dispatch({ type: "FETCH_ERROR" });
       });
   }, []);
+
   return (
     <div className="w-100">
       <Container className="w-100" fluid>
-        <Row className="justify-content-around">
-          <Col md="8">TaskManager</Col>
+        <Row className="justify-content-between ">
+          <Col md="4">TaskManager</Col>
+          <p>completed</p>
+          <Form.Check checked={state.filterCompleted} onChange={() => dispatch({ type: "COMPLETED_TOGGLE" })}></Form.Check>
+          <Col>
+            <Form.Control value={state.searchValue} onChange={(e) => dispatch({ type: "SEARCH_VALUE", newValue: e.target.value })} ></Form.Control>
+            <Button onClick={searchTasks}></Button>
+          </Col>
           <Col className="d-flex align-content-center" md="2">
-            <Button
-              onClick={() => {
-                dispatch({ type: "MODAL_TOGGLE" });
-              }}
-            >
-              Add Task
-            </Button>
+            <Form.Control value={state.createInputValue} onChange={(e) => dispatch({ type: "CREATE_INPUT_VALUE", newValue: e.target.value })}></Form.Control>
+            <Button onClick={() => submitHandler()}></Button>
           </Col>
         </Row>
         <Row>
-          <Tasks list={state}></Tasks>
+          <Tasks deleteTask={deleteTask} idGet={updateModal} list={state}></Tasks>
         </Row>
       </Container>
-      <Modal
-        show={state.modal}
-        onHide={() => dispatch({ type: "MODAL_TOGGLE" })}
-      >
+      <Modal show={state.modal} onHide={() => dispatch({ type: "MODAL_TOGGLE" })}>
         <Modal.Header>
-          <Modal.Title>Add task</Modal.Title>
+          <Modal.Title>Change Task</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={submitHandler}>
-            <InputGroup>
-              <Form.Control
-                value={state.modalInput}
-                onChange={(e) => {
-                  dispatch({ type: "VALUE", newValue: e.target.value });
-                }}
-              ></Form.Control>
-            </InputGroup>
-          </Form>
+          <Form.Control value={state.updateInputValue} onChange={(e) => dispatch({ type: "UPDATE_INPUT_VALUE", newValue: e.target.value })}></Form.Control>
+          <Form.Check checked={state.updateCheckValue} onChange={(e) => dispatch({ type: "UPDATE_CHECK_VALUE", newValue: e.target.checked })}></Form.Check>
+          <Button onClick={() => updateHandler()}>Change</Button>
         </Modal.Body>
       </Modal>
     </div>
